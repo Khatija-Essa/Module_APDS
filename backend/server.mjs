59e4676f8220de9b67550c05cff963941cc03b3d
lab_services_student <ST10029778@vcconnect.edu.za>
2024-08-08 09:37:02 +0200

    commit

---
+++ backend/server.mjs
@@ -0,0 +1,37 @@
+import https from "https";
+import http from "http";
+import fs from "fs";
+import posts from "./routes/post.mjs";
+import users from "./routes/user.mjs";
+import express from "express";
+import cors from "cors";
+
+const PORT = 3000;
+const app = express();
+
+const options = {
+  key: fs.readFileSync('keys/privateKey.pem'),
+  cert: fs.readFileSync('keys/certificate.pem')
+}
+
+app.use(cors());
+app.use(express.json());
+
+app.use((reg, res, next) =>
+ {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Methods', '*');
+  next();
+});
+
+// Routes setup
+app.use("/post", posts);
+app.route("/post",posts);
+app.use("/user",users);
+app.route("/user",users);
+
+// Create HTTPS server and start listening
+let server = http.createServer(options,app);
+
+server.listen(PORT);
